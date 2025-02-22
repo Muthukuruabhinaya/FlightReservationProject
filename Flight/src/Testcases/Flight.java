eaacb6ee6a914eb27fc250c35f3b32ec03911039
ASUS <muppenaveenkumar@gmail.com>
2025-02-22 18:16:58 +0530

    Testcase added

---
+++ Flight/src/Testcases/Flight.java
@@ -0,0 +1,18 @@
+package Testcases;
+
+public class Flight {
+
+	private static String value;
+
+	public static void main(String[] args) {
+		
+		
+	String Flight = value;
+	String Flt = Flight;
+	 System.out.println(Flt);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
